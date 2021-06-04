fc445caa55d02de3ac6d2965cb5e32a8bfc6d273
Ze Nildo Pereira <xnildo.dev@gmail.com>
2021-06-04 19:15:06 -0300

    Exe04 arrumado

--- src/aula03/Exercicio04.java
+++ src/aula03/Exercicio04.java
@@ -6,50 +6,61 @@ public class Exercicio04 {
 
 	public static void main(String[] args) {
 		Scanner ler = new Scanner(System.in);
-		int idade=0, pCalmas=0, pAgressivas=0,pNervosas=0, fem=0, masc=0, outros=0;
-		int sexo=0, personalidade=0, qtdPessoas=1, calmasMenor18=0 ;
-		int mNervosa=0, hAgressivo=0, outrosCalmo=0, nervosaMaior40=0;
-		while(qtdPessoas <= 3) {
+		int idade, sexo, humor, qtdP=0 ;
+		
+		int pCalma=0,mulhereNervosa=0, homemAgressivo=0,outrosCalmo=0;
+		
+		int nervosaMaior40=0,calmasMenor18=0 ;
+		
+		while(qtdP <= 6) {
+			System.out.println("------------------------------------------------");
 			System.out.println("Digite sua idade: ");
 			idade = ler.nextInt();
-			System.out.println("------------------------------------------------");
+			
 			
 			System.out.println("Qual seu sexo?\n[1]:FEMININO       [2]:MASCULINO       [3]:OUTROS");		
 			sexo = ler.nextInt();			
-			System.out.println("------------------------------------------------");
+			
 			
 			System.out.println("Qual sua Personalidade?\n[1]:CALMO/A       [2]:NERVOSO/A      [3]:AGRESSIVO/A  ");
-			idade = ler.nextInt();
-			System.out.println("------------------------------------------------");
-			qtdPessoas++;
+			humor = ler.nextInt();
+						
 			
-			if(personalidade == 1) {
-				pCalmas++;				
+			if(humor == 1) {
+				pCalma++;	//pessoas calmas			
 			}
-			if(personalidade == 1 || sexo ==3) {				
-				outrosCalmo++;								
+			if(sexo == 1 && humor == 2  ) {				
+				mulhereNervosa++;	//mulheres nervosas							
 			}
-			if(personalidade == 2 || sexo==1) {				
-					mNervosa++;
-			}
-			if(idade >=40 || personalidade == 2) {				
-					nervosaMaior40++;
+			if(sexo==2 &&  humor == 3  ) {				
+				homemAgressivo++;  //homens agressivos
 			}									
-			if(personalidade == 3 || sexo==1) {				
-					hAgressivo++;											
+			if(humor == 1 && sexo==3) {				
+				outrosCalmo++;	//pessoas de outros sexo calmas										
 			}
-			if(personalidade == 1 || idade < 18) {
-				calmasMenor18++;				
+			if(humor == 2 && idade > 40) {
+				nervosaMaior40++;	//pessoas nervosas com mais de 40 anos			
 			}
+			if(humor == 1 && idade < 18) {
+				calmasMenor18++;	//pessoas calmas menores de 18 anos			
+			}
+			qtdP++;
 			
 			
 		}
-		System.out.println("Nº de Pessoas Calmas: "+pCalmas);
-		System.out.println("Nº de Mulheres Nervosas: "+mNervosa);
-		System.out.println("Nº de Homens Agressivos: "+hAgressivo);
+		System.out.println("------------------------------------------------");
+		System.out.println("Nº de Pessoas Calmas: "+pCalma);
+		System.out.println("------------------------------------------------");
+		System.out.println("Nº de Mulheres Nervosas: "+mulhereNervosa);
+		System.out.println("------------------------------------------------");
+		System.out.println("Nº de Homens Agressivos: "+homemAgressivo);
+		System.out.println("------------------------------------------------");
 		System.out.println("Nº de Pessoas de outros Sexos Calmas: "+outrosCalmo);
+		System.out.println("------------------------------------------------");
 		System.out.println("Nº de Pessoas Maiores de 40 Nervosas: "+nervosaMaior40);
+		System.out.println("------------------------------------------------");
 		System.out.println("Nº de Pessoas Menores de 18 Calmas: "+calmasMenor18);
+		System.out.println("------------------------------------------------");
 	}
 
 }
